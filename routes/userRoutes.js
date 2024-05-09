023b07de6aee01f82589e0a473b61094313f315a
sarbottam_ <sarbottamstha@gmail.com>
2024-05-09 13:30:41 +1000

    refixing the environment

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let userController = require('../controllers/userController');
-
-// defining route for user request
-router.get('/', userController.getUsers);
-
-module.exports = router;    
