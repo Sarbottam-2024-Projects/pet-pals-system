4387bfef8bbef2db0211b70886badc113da4775b
TejasVarunBaskar <11tejas07@gmail.com>
2024-05-27 11:25:02 +1000

    Revert "git commit"
    This reverts commit 25f1579a130192dd52f82c6135d55c4379c0589f.

--- models/user.js
+++ models/user.js
@@ -1,11 +1,8 @@
-// user.js
-
 const { client } = require('../dbConnection.js');
-const mongoose = require('mongoose');
 
 const getUserCredentials = async (email) => {
   try {
-    let query = { email: email };
+    let query = { email: email};
     await client.connect();
     let collection = client.db("TestDB").collection("users");
     let result = await collection.findOne(query);
@@ -17,15 +14,17 @@ const getUserCredentials = async (email) => {
   }
 };
 
+
+
 const addUser = async (full_name, email, password, contact_number, role) => {
   try {
     await client.connect();
     let collection = client.db("TestDB").collection("users");
 
-    let newUser = {
+    let newUser = { 
       full_name: full_name,
       email: email,
-      password: password,
+      password: password, 
       contact_number: contact_number,
       creation_date: new Date(),
       role: role
@@ -33,7 +32,7 @@ const addUser = async (full_name, email, password, contact_number, role) => {
     let result = await collection.insertOne(newUser);
     return result;
   } catch (error) {
-    throw error;
+      throw error;
   }
 };
 
@@ -50,8 +49,8 @@ const updateUserProfile = async (email, full_name, contact_number, address, stat
     let collection = client.db("TestDB").collection("users");
 
     let result = await collection.updateOne(
-      { email: email },
-      { $set: attributes }
+      { email: email},
+      { $set: attributes}
     );
     return result;
   } catch (error) {
@@ -59,35 +58,8 @@ const updateUserProfile = async (email, full_name, contact_number, address, stat
   }
 };
 
-// Define the Pets model
-const petsSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  breed: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  }
-});
-
-const Pets = mongoose.model('Pets', petsSchema);
-
-module.exports = {
+module.exports = { 
   getUserCredentials,
   addUser,
-  updateUserProfile,
-  Pets: Pets // Export the Pets model
+  updateUserProfile
 };
